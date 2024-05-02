9773d3bc0de1d88033ce688a975a70d5ab28bc58
ErenKARSLI <karslieren61@gmail.com>
2024-05-02 23:16:04 +0300

    style3

---
+++ src/components/04-styles/style3.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./style3.css"
+
+const Style3 = () => {
+  return (
+    <div className="react-hooks">
+        <h3>Reack Hook</h3>
+            <ul>
+                <li>State</li>
+                <li>Connet</li>
+                <li>Ref</li>
+            </ul>
+    </div>
+  )
+}
+
+export default Style3
