00eed850dc23257b82a6b1d1dab3e428e7963d95
ErenKARSLI <karslieren61@gmail.com>
2024-05-02 23:15:24 +0300

    Jsx5 & 6

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-/* 
+
 import Hw from "./components/01-hello-world/hello-world"
 import Jsx1 from "./components/03-jsx/jsx1"
 import Jsx2 from "./components/03-jsx/jsx2";
 import Jsx3 from "./components/03-jsx/jsx3"; 
 import Jsx4 from "./components/03-jsx/jsx4";
+import Jsx5 from "./components/03-jsx/jsx5";
 
-*/
 
 /* kisa yolu `shift alt a` */
 
 
 
-import Jsx5 from "./components/03-jsx/jsx5";
+import Jsx6 from "./components/03-jsx/jsx6";
+import Style1 from "./components/04-styles/style1";
+import Style2 from "./components/04-styles/style2";
+import Style3 from "./components/04-styles/style3";
+import Style4 from "./components/04-styles/style4";
 
 const App = () => {
   return(
     <div>
-{/*   <Hw/>
-      <Jsx1/>
-      <Jsx2/>
-      <Jsx3/>
-      <Jsx4/>*/ }
-      <Jsx5/>
+      <Style4/>
     </div>
   )
 }
--- src/components/03-jsx/jsx5.js
+++ src/components/03-jsx/jsx5.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Jsx5 = () => {
 
       const names = ["Ali","Veli","Ayse","Fatma"]
-
+      const cities =["Istanbul","Izmir","London","Paris","Trabzon"]
 
 
   return (
@@ -12,6 +12,13 @@ const Jsx5 = () => {
           {names.map((x,i) => <li key={i}>{x}</li>)}
 
       </ul>
+
+    <select>
+      {cities.map( (city, i)=> <option key={i}> {city} </option>  )}
+
+
+    </select>
+
     </div>
   )
 }
