baed89e2ff5d4dfc28424fd68926b2606419f29d
ErenKARSLI <karslieren61@gmail.com>
2024-05-03 22:43:21 +0300

    props2

---
+++ src/components/06-props-2/products.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ProductCard from './productCard'
+
+
+const Products = () => {
+  return (
+    <div>
+        <ProductCard>
+            <h2>Merhaba        </h2>
+            <p>
+              Lorem...
+            </p>
+        </ProductCard>
+
+        <ProductCard>
+            <h2>    Hello    </h2>
+            <p>
+              Lorem.
+            </p>
+        </ProductCard>
+    </div>
+  )
+}
+
+export default Products
