4f14c7420a03e4a261cfd573b36c31c9da84941d
ErenKARSLI <karslieren61@gmail.com>
2024-05-03 22:42:54 +0300

    change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import Hw from "./components/01-hello-world/hello-world"
+/* import Hw from "./components/01-hello-world/hello-world"
 import Jsx1 from "./components/03-jsx/jsx1"
 import Jsx2 from "./components/03-jsx/jsx2";
 import Jsx3 from "./components/03-jsx/jsx3"; 
@@ -7,7 +7,7 @@ import Jsx4 from "./components/03-jsx/jsx4";
 import Jsx5 from "./components/03-jsx/jsx5";
 
 
-/* kisa yolu `shift alt a` */
+/* kisa yolu `shift alt a` 
 
 
 
@@ -15,12 +15,14 @@ import Jsx6 from "./components/03-jsx/jsx6";
 import Style1 from "./components/04-styles/style1";
 import Style2 from "./components/04-styles/style2";
 import Style3 from "./components/04-styles/style3";
-import Style4 from "./components/04-styles/style4";
+import Style4 from "./components/04-styles/style4"; 
+import Greetings from "./components/05-props-1/greetings";*/
+import Products from "./components/06-props-2/products";
 
 const App = () => {
   return(
     <div>
-      <Style4/>
+      <Products/>
     </div>
   )
 }
