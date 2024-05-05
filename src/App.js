536aefba7a3750e1360252125c93fe9708910263
ErenKARSLI <karslieren61@gmail.com>
2024-05-05 16:23:05 +0300

    assets

--- src/App.js
+++ src/App.js
@@ -17,13 +17,15 @@ import Style2 from "./components/04-styles/style2";
 import Style3 from "./components/04-styles/style3";
 import Style4 from "./components/04-styles/style4"; 
 import Greetings from "./components/05-props-1/greetings";
-import Products from "./components/06-props-2/products";*/
+import Products from "./components/06-props-2/products";
 import Image from "./components/09-image/image";
-
+import DijitalSaatUygulamasi1 from "./components/10-digital-saat-1/dijital-saat-uygulamasi1"
+import ProfileCard from "./components/08-profile-card/profile-card";
+import DijitalSaatUygulamasi2 from "./components/11-dijital-saat-uygulamasi2/dijital-saat-uygulamasi2";;*/
 const App = () => {
   return(
     <div>
-      <Image/>
+      <ProfileCard/>
     </div>
   )
 }
