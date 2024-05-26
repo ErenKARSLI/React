9d5e54a0693866b97f21e0af993ef3f9c3689c15
ErenKARSLI <karslieren61@gmail.com>
2024-05-26 16:55:12 +0300

    change

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
+import AppRouter from "./router/router";
+import "./assets/scss/style.scss";
+import AppRouter2 from "./router/router2";
 
-/* import Hw from "./components/01-hello-world/hello-world"
-import Jsx1 from "./components/03-jsx/jsx1"
-import Jsx2 from "./components/03-jsx/jsx2";
-import Jsx3 from "./components/03-jsx/jsx3"; 
-import Jsx4 from "./components/03-jsx/jsx4";
-import Jsx5 from "./components/03-jsx/jsx5";
-
-
-/* kisa yolu `shift alt a` 
-
-
-
-import Jsx6 from "./components/03-jsx/jsx6";
-import Style1 from "./components/04-styles/style1";
-import Style2 from "./components/04-styles/style2";
-import Style3 from "./components/04-styles/style3";
-import Style4 from "./components/04-styles/style4"; 
-import Greetings from "./components/05-props-1/greetings";
-import Products from "./components/06-props-2/products";
-import Image from "./components/09-image/image";
-import DijitalSaatUygulamasi1 from "./components/10-digital-saat-1/dijital-saat-uygulamasi1"
-import ProfileCard from "./components/08-profile-card/profile-card";
-import DijitalSaatUygulamasi2 from "./components/11-dijital-saat-uygulamasi2/dijital-saat-uygulamasi2";;*/
-const App = () => {
-  return(
-    <div>
-      <ProfileCard/>
-    </div>
-  )
-}
-export default App;
-
-
+export default function App() {
+    return (
+        <>
+            {/* <AppRouter /> */}
+            <AppRouter2 />
+        </>
+    )
+};
